4ad7a050c83929551f90833d0596d9616d09a8f1
Perry Eising <perry.r.eising@gmail.com>
2018-05-25 12:15:27 -0700

    transition to help queue, add properties

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
+var heading = React.createElement('h1', {}, 'Help Queue');
+var ticketLocation = React.createElement('h3', {},'3a');
+var ticketNames = React.createElement('h3', {}, 'Perry and Lena');
+var ticketIssue = React.createElement('h3',{}, "anxious");
 
+var app = React.createElement('div', {}, heading, ticketLocation, ticketNames, ticketIssue);
 
-function liveTime(){
+ReactDOM.render(
+  app,
+  document.getElementById('react-app-root')
+);
 
-  var greeting = React.createElement('h1', {}, 'Hello, World!');
-
-  var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
-
-  var app = React.createElement('div', {}, greeting, clock);
   console.log("update");
   ReactDOM.render(
     app,
     document.getElementById('react-app-root')
   );
-}
-
-setInterval (liveTime, 1000);
