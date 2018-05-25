8028a47a545dfc7d16bc20557a7d68ff4b66d681
Perry Eising <perry.r.eising@gmail.com>
2018-05-25 12:30:25 -0700

    transition to jsx, babel outstanding

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+ReactDOM.render(
+  <div>
+    <h1>Help Queue</h1>
+    <h3>3a</h3>
+    <h3>Perry and Lena</h3>
+    <h3>react is broken</h3>
+  </div>,
+      document.getElementById('react-app-root')
+);
