945a34783f199fb3f799ac6a5fff10fbb8aaf176
Perry Eising <perry.r.eising@gmail.com>
2018-05-25 14:29:21 -0700

    Add header, ticketlist and ticket components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
 
 function App(){
   return (
     <div>
-      <h1>Help Queue</h1>
-      <h3>3a</h3>
-      <h3>Perry and Lena</h3>
-      <h3>react is broken??</h3>
-      <hr/>
+      <Header/>
+      <TicketList/>
     </div>
   );
 }
---
+++ src/components/Ticket.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Ticket(){
+  return (
+    <div>
+      <h3>3a</h3>
+      <h3>Perry and Lena</h3>
+      <h3>react is broken??</h3>
+      <hr/>
+    </div>
+  );
+}
+
+export default Ticket;
