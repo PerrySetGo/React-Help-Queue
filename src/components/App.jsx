e1a11e0c3e22840c98ec87df2f70e991ddb4d7ee
Perry Eising <perry.r.eising@gmail.com>
2018-05-25 14:17:33 -0700

    add first component and change index.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App(){
+  return (
+    <div>
+      <h1>Help Queue</h1>
+      <h3>3a</h3>
+      <h3>Perry and Lena</h3>
+      <h3>react is broken??</h3>
+      <hr/>
+    </div>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
 ReactDOM.render(
-  <div>
-    <h1>Help Queue</h1>
-    <h3>3a</h3>
-    <h3>Perry and Lena</h3>
-    <h3>react is broken</h3>
-  </div>,
-      document.getElementById('react-app-root')
+  <App/>,
+  document.getElementById('react-app-root')
 );
