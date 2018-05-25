7414835ae401f4e5d4aaf6cc426a7d0ed8dbcc17
Perry Eising <perry.r.eising@gmail.com>
2018-05-25 11:54:58 -0700

    wrap greeting and clock in livetime method and print to console

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-var greeting = React.createElement('h1', {}, 'Hello, World!');
 
-var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
 
-var app = React.createElement('div', {}, greeting, clock);
+function liveTime(){
 
-ReactDOM.render(
-  app,
-  document.getElementById('react-app-root')
-);
+  var greeting = React.createElement('h1', {}, 'Hello, World!');
+
+  var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+
+  var app = React.createElement('div', {}, greeting, clock);
+  console.log("update");
+  ReactDOM.render(
+    app,
+    document.getElementById('react-app-root')
+  );
+}
+
+setInterval (liveTime, 1000);
